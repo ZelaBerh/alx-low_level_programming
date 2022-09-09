b858528babc4d679e7d0699eb3ec9a4b9a1d8cfc
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 17:57:21 +0300

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
