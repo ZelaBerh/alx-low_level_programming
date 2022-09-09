83d9e6e513a036ebdec2e8dd90173796fde95935
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 15:01:46 +0300

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabet
+ *
+ * Return: 0
+ */
+
+ int main()
+ {
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(letters);
+
+	return (0);
+ }
