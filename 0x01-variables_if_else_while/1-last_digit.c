a0d5eba3ecd233fba8203e55d5c62cab6e49cc1c
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 14:51:05 +0300

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,28 +12,28 @@
 
 int main(void)
 {
-    int n, last_digit;
-    char greater[50] = "and is greater than 5";
-    char less[50] = "and is less than 6 and not 0";
-    char zero[50] = "and is 0";
+	int n, last_digit;
+	char greater[50] = "and is greater than 5";
+	char less[50] = "and is less than 6 and not 0";
+	char zero[50] = "and is 0";
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    last_digit = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-    if (last_digit > 5)
-    {
-        printf("Last digit of %d is %d %s\n", n, last_digit, greater);
-    }
-    else if (last_digit < 6 && last_digit != 0)
-    {
-        printf("Last digit of %d is %d %s\n", n, last_digit, less);
-    }
-    else
-    {
-        printf("Last digit of %d is %d %s\n", n, last_digit, zero);
-    }
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d %s\n", n, last_digit, greater);
+	}
+	else if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %d is %d %s\n", n, last_digit, less);
+	}
+	else
+	{
+		printf("Last digit of %d is %d %s\n", n, last_digit, zero);
+	}
 
-    return (0);
+	return (0);
 
 }
