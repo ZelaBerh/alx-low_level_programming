2d87ec72cf4772560a6fa5a8857da918cd615424
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 16:39:20 +0300

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,13 @@
  * Return: 0
  */
 
- int main(void)
- {
+int main(void)
+{
 	int x;
-	for(x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 	putchar(x);
 	}
 	putchar('\n');
 	return (0);
- }
+}
