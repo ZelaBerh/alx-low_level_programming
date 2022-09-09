7d63d2569161f477209e17b063aecca6475c4e84
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 23:03:24 +0300

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints combination of numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
