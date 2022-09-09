783cfb1f0a9f7ef91a3cd985dc32549a3089b4b5
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 17:22:44 +0300

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabet
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+
+	int ch = 1;
+
+	for (ch = 0; ch <= 9; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
