d6f500da5dff749a11c340a1b54b22b897eed0f9
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 23:59:17 +0300

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print combo
+ *
+ * Description: print combo of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
