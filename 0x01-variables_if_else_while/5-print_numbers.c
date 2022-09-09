4f91e999997f49d5a329736c5fd8e0f7d1736c40
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-09 16:55:12 +0300

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
