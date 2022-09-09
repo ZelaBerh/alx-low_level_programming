e805128cd3271ece6985fc8cf7530b8bdc49acb1
ZelaBerh <zemberhanu@gmail.com>
2022-09-08 19:17:28 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: shows sizes of file types
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+
+	return (0);
+}
