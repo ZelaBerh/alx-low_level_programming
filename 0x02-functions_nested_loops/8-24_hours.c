b157b54faa833dd87b7553c9c9b6f165ce2e61fb
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-14 20:47:01 +0300

    update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * jack_bauer - absolute value
+ *
+ * Return: Always 0.
+ */
+
+
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 1; i <= 24*60; i++)
+	{
+		_putchar(i + '0');
+	}
+}
