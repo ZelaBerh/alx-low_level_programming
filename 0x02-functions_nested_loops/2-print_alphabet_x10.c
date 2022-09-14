13e108342ec6f9db6ba1fa6ea26fb95046eadd29
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-14 19:25:51 +0300

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,11 @@ void print_alphabet_x10(void)
 {
 	char text[26] = "abcdefghijklmnopqrstuvwxyz";
 	int j;
-	for(j = 1; j < 11; j++)
+
+	for( j = 1; j < 11; j++)
 	{
 		int i = 0;
+		
 		while (i < 26)
 			{
 				_putchar(text[i]);
