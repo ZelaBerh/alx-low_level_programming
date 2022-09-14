6f5a984d3ac4c00572c898e2e5365169cda692c7
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 00:07:44 +0300

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,17 @@ void print_to_98(int n)
 
 	int i;
 
-	if (n<=98)
+	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
 			if (i == 98)
 			{
-				printf("%d ",i);
+				printf("%d ", i);
 			}
 			else
 			{
-				printf("%d, ",i);
+				printf("%d, ", i);
 			}
 		}
 	}
@@ -33,11 +33,11 @@ void print_to_98(int n)
 		{
 			if (i == 98)
 			{
-				printf("%d ",i);
+				printf("%d ", i);
 			}
 			else
 			{
-				printf("%d, ",i);
+				printf("%d, ", i);
 			}
 		}
 	}
