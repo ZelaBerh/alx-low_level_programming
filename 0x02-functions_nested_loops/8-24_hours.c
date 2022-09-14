ab2b9ad6c9fc46a1afb876273aa2dfb68163d9a8
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-14 21:51:12 +0300

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include "main.h"
 
 /**
@@ -10,23 +13,29 @@
 void jack_bauer(void)
 {
 	int i,j,k,l;
+	bool stop = false;
 
-	for (i = 0; i <3 ; i++)
+	for (i = 0; i <3 && !stop ; i++)
 	{
-		for (j = 0; j <10 ; j++)
+		for (j = 0; j <10 && !stop; j++)
 		{
-			for (k = 0; k <6 ; k++)
+			for (k = 0; k <6 && !stop; k++)
 			{
-				for (l = 0; l <10 ; l++)
+				for (l = 0; l <10 && !stop; l++)
 				{
 					_putchar(i + '0');
 					_putchar(j + '0');
-			        _putchar(':');
+					_putchar(':');
 					_putchar(k + '0');
 					_putchar(l + '0');
 					_putchar('\n');
+					if (i==2 && j==3 && k==5 && l==9)
+					{
+						stop = true;
+					}
 				}
 			}
 		}
 	}
+	return 0;
 }
