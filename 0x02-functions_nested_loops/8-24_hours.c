1c037b418179b6fdd7d75c35a9dcebc438e65789
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-14 21:15:43 +0300

    up

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,10 +9,24 @@
 
 void jack_bauer(void)
 {
-	int i;
+	int i,j,k,l;
 
-	for (i = 1; i <= 24*60; i++)
+	for (i = 0; i <3 ; i++)
 	{
-		_putchar(i + '0');
+		for (j = 0; j <10 ; j++)
+		{
+			for (k = 0; k <6 ; k++)
+			{
+				for (l = 0; l <10 ; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+			        _putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
 	}
 }
