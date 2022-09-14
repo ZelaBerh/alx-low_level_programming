83502d489930ba030f4eb94c520cfa359802388f
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 00:11:26 +0300

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		{
 			if (i == 98)
 			{
-				printf("%d \n", i);
+				printf("%d\n", i);
 			}
 			else
 			{
@@ -33,7 +33,7 @@ void print_to_98(int n)
 		{
 			if (i == 98)
 			{
-				printf("%d \n", i);
+				printf("%d\n", i);
 			}
 			else
 			{
