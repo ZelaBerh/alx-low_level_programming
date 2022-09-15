5ccb50e811c552d8ea46218d1e3287f69798ca81
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 22:41:00 +0300

    nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Return: numbers
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+
+	for ( i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
