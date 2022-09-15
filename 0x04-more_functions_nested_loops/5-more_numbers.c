1a7d012261ab1cde60b2a39bd0633a8b36a7b6dc
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 23:11:06 +0300

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	int tens = j / 10;
-	int ones = j % 10;
+	int tens, ones;
+
 
 	for ( i = 1; i <= 10; i++)
 	{
 		for ( j = 0; j <= 14; j++)
 		{
+			tens = j / 10;
+			ones = j % 10;
 			_putchar(tens + '0');
 			_putchar(ones + '0');
 		}
