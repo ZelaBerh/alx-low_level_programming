49a80d15e24ad23beacc5d9990847adb5ed8dacf
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 22:33:20 +0300

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiply
+ *
+ * @a: takes input
+ * @b: takes input
+ *
+ * Return: a*b
+ */
+
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
