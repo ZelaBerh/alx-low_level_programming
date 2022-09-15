c9f02f7a4a5b99a643917025aa4229b14e410e06
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 23:24:28 +0300

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines
+ *
+ * Description: prints lines
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+
+	int i;
+
+	if (n > 0)
+	{
+		for ( i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	
+}
