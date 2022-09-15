0cb3fa4e8e3eae04e69e678a1f5ed11fb13d4fad
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 23:09:12 +0300

    nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints until 14
+ *
+ * Description: prints until 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	int tens = j / 10;
+	int ones = j % 10;
+
+	for ( i = 1; i <= 10; i++)
+	{
+		for ( j = 0; j <= 14; j++)
+		{
+			_putchar(tens + '0');
+			_putchar(ones + '0');
+		}
+		_putchar('\n');
+	}
+}
