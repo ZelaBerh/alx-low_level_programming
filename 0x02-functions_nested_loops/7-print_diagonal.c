b20cdbbc6c9d718a90afce48ce8ab1ad199dc4bc
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 23:37:29 +0300

    update

---
+++ 0x02-functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints disgonal
+ *
+ * Description: prints lines
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for ( i = 0; i <= n; i++)
+		{
+			_putchar('\');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	
+}
