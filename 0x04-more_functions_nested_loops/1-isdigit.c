5ac1709729591d1ed7fbbdc1bec324ff264156de
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 22:12:31 +0300

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isdigit - digit
+ *
+ * @c: takes c input
+ *
+ * Return: Always 0.
+ */
+
+
+
+int _isdigit(int c)
+{
+	if (c > 0 && c <= 0 )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
