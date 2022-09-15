9b86a23ea5caedff0bbbcd8ede767a1ffecc330b
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-15 22:51:06 +0300

    num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9
+ *
+ * Return: numbers
+ */
+
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for ( i = 0; i <= 9; i++)
+	{
+		if ( i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+			_putchar('\n');
+		}
+		
+	}
+}
