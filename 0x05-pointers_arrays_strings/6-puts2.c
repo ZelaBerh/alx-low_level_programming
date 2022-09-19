0efa953e74a52d36156816f1078783f9eda7d5e3
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-19 12:04:43 +0300

    l

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,19 +12,15 @@ void puts2(char *str)
 
 char *ptr;
 int i;
-int count = 0;
+
 
 ptr = str;
 
-while (*str != '\0')
-{
-count++;
-str++;
-}
+
 
 for (i = 0; ptr[i] != '\0'; i = i + 2)
 {
-if (count >= 2 && ptr[i] == 'H')
+if (i >= 12 && ptr[i] == 'H')
 	break;
 _putchar(ptr[i]);
 
