5e41621e00c02875bfca167d1cc85eed912252d6
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-19 10:17:08 +0300

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - string length
+* @s: pointer to int
+* Return: void
+*/
+
+int _strlen(char *s)
+{
+int count = 0;
+while (*s != '\0') 
+{
+count++;
+s++;
+}
+return count;
+}
