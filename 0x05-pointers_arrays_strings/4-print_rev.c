349712e9ba0d3f13afa83037d77d0b09ab1d1b8e
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-19 22:17:51 +0300

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,53 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
-* print_rev - print s in reverse
-* @s: pointer to int
-* Return: void
-*/
-
- 
-int string_length(char *pointer)
-{
-int c = 0;
-
-while( *(pointer+c) != '\0' )
-{
-c++;
-}
-
-return c;
-}
-
+ * print_rev - reverse a string
+ * @s: string
+ * Return: void
+ */
 
 void print_rev(char *s)
 {
+	int len = strlen(s);
+	int i;
 
-int length, c;
-
-char *begin, *end, temp;
-
-length = string_length(s);
-
-begin = s;
-end = s;
-
-for ( c = 0 ; c < ( length - 1 ) ; c++ )
-{
-end++;
+	for (i = len - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar(10);
 }
-
-for ( c = 0 ; c < length/2 ; c++ ) 
-{
-temp = *end;
-*end = *begin;
-*begin = temp;
-
-begin++;
-end--;
-}
-
-
-}
-
