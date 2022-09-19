a665ab22dbd711facb5c8b309a403fc9e41d8d38
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-19 11:33:48 +0300

    l

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,26 +11,26 @@ void print_rev(char *s)
 {
 
 int length, c;
-   char *begin, *end, temp;
- 
-   length = string_length(s);
- 
-   begin = s;
-   end = s;
- 
-   for ( c = 0 ; c < ( length - 1 ) ; c++ )
-      end++;
- 
-   for ( c = 0 ; c < length/2 ; c++ ) 
-   {        
-      temp = *end;
-      *end = *begin;
-      *begin = temp;
- 
-      begin++;
-      end--;
-   }
+
+char *begin, *end, temp;
+
+length = string_length(s);
+
+begin = s;
+end = s;
+
+for ( c = 0 ; c < ( length - 1 ) ; c++ )
+end++;
+
+for ( c = 0 ; c < length/2 ; c++ ) 
+{
+temp = *end;
+*end = *begin;
+*begin = temp;
+
+begin++;
+end--;
+}
 _putchar('\n');
 
 }
-
