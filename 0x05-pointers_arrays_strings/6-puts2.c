8724a5eb9a8b6ea9ff06f67df4abacc68fbf8b8b
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-19 11:50:46 +0300

    every other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _puts2 - print every other
+* @str: pointer to int
+* Return: void
+*/
+
+void puts2(char *str)
+{
+
+char *ptr;
+int i;
+
+ptr = str;
+
+for (i = 0; ptr[i] != '\0'; i = i +2)
+{
+_putchar(ptr[i]);
+}
+_putchar('\n');
+
+}
