3118b7e9a7493b52c554cb6744dc007b90c3a594
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-19 12:50:16 +0300

    p

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,14 +16,23 @@ char *ptr;
 
 ptr = src;
 
-for ( i = 0; ptr[i] != '\0'; i++)
+for (i = 0; ptr[i] != '\0'; i++)
 {
 	dest[i] = ptr[i];
+	if (ptr[i] == '\0')
+	{
+		dest[i+1] = ptr[i+1];
+	}
 }
 
-for ( i = 0; ptr[i] != '\0'; i++)
+for (i = 0; ptr[i] != '\0'; i++)
 {
 	_putchar(dest[i]);
+	if (ptr[i] == '\0')
+	{
+		dest[i+1] = ptr[i+1];
+		_putchar(dest[i+1]);
+	}
 }
 
 return (0);
