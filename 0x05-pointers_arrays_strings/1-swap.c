0d6327448986b79c232ff429b5d4ef32dabd7e4f
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-19 09:14:58 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* swap_int - swap numbers
+* @a: pointer to int
+* @b: pointer to int
+* @c: intermediary
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+
+}
