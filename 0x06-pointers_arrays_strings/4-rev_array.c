dd1736fd87f5de81fdcd57dd7ec5769219f1b118
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-23 01:04:31 +0300

    update

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse array
+ * @a: array
+ * @n: number of elements
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int half = n / 2;
+	int left, right, i;
+
+	n -= 1;
+
+	for (i = 0; i < half; i++)
+	{
+		left = a[n - i];
+		right = a[i];
+		a[i] = left;
+		a[n - i] = right;
+	}
+}
