3cba024bf6717040c80b968f8a5bb04abdbf5147
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-26 21:53:23 +0300

    update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,20 +1,2 @@
-#include "main.h"
-
-/**
- * _memset - fills memory with a constant byte
- * @s: string array
- * @b: constant byte
- * @n: number of bytes
- * Return: pointer
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i = 0;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
-}
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
