eba8326aa58b35d8e21a7f87034678b3a8e35f08
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-28 23:38:33 +0300

    u

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - string length
+ * @s: string
+ * Return: pointer
+ */
+
+
+
+
+int _strlen_recursion(char *s)
+{
+
+	int i = 0;
+
+	if ( *s != '\0')
+	{
+		i++;
+		_strlen_recursion(s+1);
+	}
+	else
+	{
+		return (i);
+	}
+}
