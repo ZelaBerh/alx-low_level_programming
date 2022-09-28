4555acc98736d69309e3aff4b8faf95df5ed3595
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-09-28 23:41:34 +0300

    ll

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -16,10 +16,10 @@ int _strlen_recursion(char *s)
 
 	int i = 0;
 
-	if ( *s != '\0')
+	if (*s != '\0')
 	{
-		i++;
-		_strlen_recursion(s+1);
+		++i;
+		_strlen_recursion(s + 1);
 	}
 	
 	return (i);
