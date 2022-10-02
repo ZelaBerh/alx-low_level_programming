73531d6e6bdff01204941725f1852dadc6fa114b
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-02 18:57:58 +0300

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: (0)
+ */
+
+
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += argv[i];
+		}
+		printf("%d", sum);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
