553699ffcc0d8868c0f5a2b6b68f0a4ba488005d
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-02 19:11:18 +0300

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,20 @@
 int main(int argc, char *argv)
 {
 int i;
-int sum = 0;
+int sum = 1;
 
 if (argc > 1)
 {
 for (i = 1; i < argc; i++)
 {
-sum += argv[i];
+sum = sum * atoi(argv[i]);
 }
 printf("%d", sum);
-return (0);
 }
 else
 {
 printf("Error");
 return (1);
 }
+return (0);
 }
