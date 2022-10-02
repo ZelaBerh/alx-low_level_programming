15ee26c801a64dfa804772e468e7035c1d375d86
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-02 21:17:55 +0300

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,21 +11,21 @@
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-        int i;
-        int mul = 1;
+		int i;
+		int mul = 1;
 
-        if (argv[1] != NULL && argv[2] != NULL)
-        {
-                for (i = 1; i < 3; i++)
-                {
-                        mul = mul * atoi(argv[i]);
-                }
-                printf("%d\n", mul);
-        }
-        else
-        {
-                printf("Error\n");
-                return (1);
-        }
-        return (0);
+		if (argv[1] != NULL && argv[2] != NULL)
+		{
+				for (i = 1; i < 3; i++)
+				{
+						mul = mul * atoi(argv[i]);
+				}
+				printf("%d\n", mul);
+		}
+		else
+		{
+				printf("Error\n");
+				return (1);
+		}
+		return (0);
 }
