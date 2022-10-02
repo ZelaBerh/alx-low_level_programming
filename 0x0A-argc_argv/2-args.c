d6ba508cf97a245cfa79b033360007bb85f33ddf
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-02 18:47:39 +0300

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,10 +12,9 @@ int main(int argc, __attribute__((unused)) char **argv)
 {
 	int i;
 	
-	for( i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
-
 	return (0);
 }
