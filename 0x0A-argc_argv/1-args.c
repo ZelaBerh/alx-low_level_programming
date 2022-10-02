df38f5d68adf91b682d427f30c9cddb5ca278591
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-02 18:33:59 +0300

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: (0)
+ */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
