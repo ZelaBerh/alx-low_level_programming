c1dfefed3e632c8c09df3a53d3b09d058d875be6
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-04 11:29:05 +0300

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - copies strings
+ * @str: string
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+	char *lin;
+	int i;
+
+	one = strlen(s1);
+	two = strlen(s2);
+
+	length = one + two;
+
+	lin = malloc(length * sizeof(char));
+
+	for (i = 0; i < one; i++)
+	{
+		*(lin + i) = *(s1 + i);
+	}
+
+	for (j = one; j < two; j++)
+	{
+		*(lin + j) = *(s2 + j);
+	}
+
+	return (lin);
+
+}
