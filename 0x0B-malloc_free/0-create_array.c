7aa09df7e11d1778275725c7e339d12c90433988
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-04 09:49:56 +0300

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,12 @@ char *create_array(unsigned int size, char c)
 char *ptr;
 unsigned int i;
 
-if(size >=1)
+if (size >= 1)
 {
-	ptr = (char*) malloc(size * sizeof(char));
+	ptr = (char *) malloc(size * sizeof(char));
 }
 
-if ( size <= 0)
+if (size <= 0)
 {
 	return (NULL);
 }
