0ca020310444cd9ee9704e75e056bc7ff481ab63
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-04 09:46:10 +0300

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - creates arrays
+ * @size: size
+ * @c: char
+ * Return: array
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+
+char *ptr;
+int i;
+
+if(size >=1)
+{
+	ptr = (char*) malloc(size * sizeof(char));
+}
+
+if ( size <= 0)
+{
+	return (NULL);
+}
+else
+{
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+}
+}
