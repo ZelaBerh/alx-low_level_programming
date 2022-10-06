d0102577426da607a3bbedfbee455d7120948d41
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-06 23:14:04 +0300

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,57 +1,36 @@
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * string_nconcat - allocates memory
- *
- * @s1: string
- * @s2: string
- * @n: number of bytes
- * Return: pointer
+ * string_nconcat - concatenates two stringa.
+ * @s1: string to be concatenated.
+ * @s2: string to be concatenated.
+ * @n: first bytes of s2.
+ * Return: String concatenated.
  */
-
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-char *ptr;
-int i, j, length, one;
-
-if (n >= strlen(s2))
-{
-n = strlen(s2);
-}
+char *array = NULL;
+unsigned int i = 0, j = 0, k;
 
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
-
-
-one = strlen(s1);
-length = one + n;
-
-ptr = malloc(length + 1);
-
-
-if (ptr == NULL)
-{
-return(NULL);
-}
-
-for (i = 0; i < one; i++)
-{
-	*(ptr + i) = *(s1 + i);
-}
-
-for (j = one; j < length; j++)
-{
-	*(ptr + j) = *(s2 + (j - one));
-}
-return (ptr);
+while (s1[i] != '\0')
+i++;
+while (s2[j] != '\0')
+j++;
+if (n < j)
+j = n;
+array = malloc(((i + j + n) * sizeof(char) + 1));
+if (array == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+array[k] = s1[k];
+i = k;
+for (k = 0; k < j; k++, i++)
+array[i] = s2[k];
+array[i] = '\0';
+return (array);
 }
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * array_range - allocates memory
+ *
+ * @max: array
+ * @min: bytes
+ * Return: pointer
+ */
+
+
+int *array_range(int min, int max)
+{
+
+}
