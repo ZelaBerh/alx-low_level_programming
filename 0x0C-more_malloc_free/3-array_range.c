bdfdd45af236753a6b658f2cba353c2cdce6b5bc
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-06 23:31:16 +0300

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,31 +2,29 @@
 #include <stdlib.h>
 
 /**
- * *array_range - is function that creates an ptr
- *@max: is value to int.
- *@min: is value to int.
+ * array_range - Creates an array of integers
+ * @min: The first value of the array.
+ * @max: The last value of the array.
  *
- * Return: the pointer to the new ptr.
-**/
+ * Return: a pointer to the newly created array.
+ */
 
 int *array_range(int min, int max)
 {
-int i;
-int *a;
+	int *array, index, size;
 
-if (min > max)
-{
-return (NULL);
-}
-a = malloc((max - min + 1) * sizeof(*a));
-if (a == NULL)
-{
-return (NULL);
-}
+	if (min > max)
+		return (NULL);
 
-for (i = 0; min <= max; i++, min++)
-{
-a[i] = min;
-}
-return (a);
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
 }
