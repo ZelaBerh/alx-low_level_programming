f08dff247427797e5480ecf9865a154f6c2d79d7
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-10 22:40:04 +0300

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable
+ *
+ * @d: dog
+ */
+
+void print_dog(struct dog *d)
+{
+
+if (d == NULL )
+return;
+
+if ((*d).name == NULL)
+printf("nil\n");
+else
+printf("Name: %s\n", d->name);
+
+if ((*d).age < 0)
+printf("nil\n");
+else
+printf("Age: %f\n", d->age);
+
+if ((*d).owner == NULL)
+printf("nil\n");
+else
+printf("Owner: %s\n", d->owner);
+
+
+}
