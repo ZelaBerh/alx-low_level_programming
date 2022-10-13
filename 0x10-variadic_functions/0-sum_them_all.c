7ceb8ae91014d7c97a7d1cc0f17c01448657173a
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-13 23:35:53 +0300

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,11 +15,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 
 va_list nums;
-unsigned int i;
-int sum = 0;
-
 va_start(nums, n);
 
+unsigned int i;
+int sum = 0;
 
 for (i = 0; i < n; i++)
 {
