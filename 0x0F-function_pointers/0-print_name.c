59af308083bc06f19abac385fe1e5b24d566c834
Zela B <111011414+ZelaBerh@users.noreply.github.com>
2022-10-14 00:36:09 +0300

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,15 @@
-m
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - prints a name from a given name.
+ * @name: string that we need to display.
+ * @f: function that is going to be called from our function.
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
